9ae6c012a65aff461634dcd962f018d7915cd240
alpha <yanjifa2013@qq.com>
2020-11-15 03:22:39 +0800

    :sparkles: preview add device support safearea

--- preview-templates/boot.js
+++ preview-templates/boot.js
@@ -4,28 +4,29 @@
 
     // init device resolutions
     var devices = [
-        { name: "Apple iPhone 5",       width: 320,  height: 568,  ratio: 2     },
-        { name: "Apple iPhone 6",       width: 375,  height: 667,  ratio: 2     },
-        { name: "Apple iPhone 6 Plus",  width: 414,  height: 736,  ratio: 3     },
-        { name: "Apple iPhone 7",       width: 375,  height: 667,  ratio: 2     },
-        { name: "Apple iPhone 7 Plus",  width: 414,  height: 736,  ratio: 3     },
-        { name: "Apple iPhone X",       width: 375,  height: 812,  ratio: 3     },
+        { name: "Xiao Mi 10 Pro", width: 540, height: 1170, ratio: 1, safeWidth: 540, safeHeight: 1125 },
+        { name: "Apple iPhone 5", width: 320, height: 568, ratio: 2 },
+        { name: "Apple iPhone 6", width: 375, height: 667, ratio: 2 },
+        { name: "Apple iPhone 6 Plus", width: 414, height: 736, ratio: 3 },
+        { name: "Apple iPhone 7", width: 375, height: 667, ratio: 2 },
+        { name: "Apple iPhone 7 Plus", width: 414, height: 736, ratio: 3 },
+        { name: "Apple iPhone X", width: 375, height: 812, ratio: 3 },
 
-        { name: "Apple iPad",               width: 1024, height: 768,  ratio: 2 },
-        { name: "Apple iPad Air 2",         width: 768,  height: 1024, ratio: 2 },
-        { name: "Apple iPad Pro 10.5-inch", width: 834,  height: 1112, ratio: 2 },
+        { name: "Apple iPad", width: 1024, height: 768, ratio: 2 },
+        { name: "Apple iPad Air 2", width: 768, height: 1024, ratio: 2 },
+        { name: "Apple iPad Pro 10.5-inch", width: 834, height: 1112, ratio: 2 },
         { name: "Apple iPad Pro 12.9-inch", width: 1024, height: 1366, ratio: 2 },
 
-        { name: "Huawei P9",            width: 540,  height: 960,  ratio: 2     },
-        { name: "Huawei Mate9 Pro",     width: 720,  height: 1280, ratio: 2     },
+        { name: "Huawei P9", width: 540, height: 960, ratio: 2 },
+        { name: "Huawei Mate9 Pro", width: 720, height: 1280, ratio: 2 },
 
-        { name: "Google Nexus 5",       width: 360,  height: 640,  ratio: 3     },
-        { name: "Google Nexus 5X",      width: 411,  height: 731,  ratio: 2.625 },
-        { name: "Google Nexus 6",       width: 412,  height: 732,  ratio: 3.5   },
-        { name: "Google Nexus 7",       width: 960,  height: 600,  ratio: 2     },
+        { name: "Google Nexus 5", width: 360, height: 640, ratio: 3 },
+        { name: "Google Nexus 5X", width: 411, height: 731, ratio: 2.625 },
+        { name: "Google Nexus 6", width: 412, height: 732, ratio: 3.5 },
+        { name: "Google Nexus 7", width: 960, height: 600, ratio: 2 },
     ];
 
-    function setCSSChecked (element, checked) {
+    function setCSSChecked(element, checked) {
         if (checked) {
             element.classList.add("checked");
         }
@@ -35,7 +36,7 @@
         return checked;
     }
 
-    function refreshPauseBtnState () {
+    function refreshPauseBtnState() {
         if (cc.game.isPaused()) {
             btnPause.style.borderTopRightRadius = "0";
             btnPause.style.borderBottomRightRadius = "0";
@@ -58,7 +59,7 @@
         var check = false;
         // eslint-disable-next-line max-len
         // eslint-disable-next-line no-useless-escape
-        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)))check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+        (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
         return check;
     };
 
@@ -80,11 +81,11 @@
     var inputSetFPS = document.getElementById("input-set-fps");
     var btnRecompile = document.getElementById("btn-recompile");
 
-    devices.forEach( function ( info, idx ) {
+    devices.forEach(function (info, idx) {
         var opt = document.createElement("option");
-        opt.value = idx+1;
+        opt.value = idx + 1;
         opt.text = info.name;
-        optsDevice.add( opt, null );
+        optsDevice.add(opt, null);
     });
 
     // coockie
@@ -92,26 +93,26 @@
     function setCookie(name, value, days) {
         // cookie will be saved for 30 days
         days = days || 30;
-        var expires  = new Date();
-        expires.setTime(expires.getTime() + days*24*60*60*1000);
-        document.cookie = name + "="+ encodeURIComponent(value) + ";expires=" + expires.toGMTString();
+        var expires = new Date();
+        expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+        document.cookie = name + "=" + encodeURIComponent(value) + ";expires=" + expires.toGMTString();
     }
 
-    function getCookie (name) {
+    function getCookie(name) {
         var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
-        if(arr !== null) return (arr[2]);
+        if (arr !== null) return (arr[2]);
         return null;
     }
 
-    function isFullScreen () {
+    function isFullScreen() {
         var toolbar = document.getElementsByClassName("toolbar")[0];
         return getComputedStyle(toolbar).display === "none";
     }
 
-    function getEmulatedScreenSize () {
-        var w, h;
+    function getEmulatedScreenSize() {
+        var w, h, sw, sh, sRatio;
         var idx = optsDevice.value;
-        if ( idx === "0" ) {
+        if (idx === "0") {
             w = designWidth;
             h = designHeight;
         }
@@ -119,14 +120,20 @@
             var info = devices[parseInt(idx) - 1];
             w = info.width;
             h = info.height;
+            sw = info.safeWidth;
+            sh = info.safeHeight;
+            sRatio = info.ratio;
         }
         return {
             width: rotated ? h : w,
             height: rotated ? w : h,
+            safeWidth: rotated ? sh : sw,
+            safeHeight: rotated ? sw : sh,
+            ratio: sRatio,
         };
     }
 
-    function showSplash () {
+    function showSplash() {
         var LOGO_IMG_L_W = 416;
         var LOGO_IMG_L_H = 87;
         var LOGO_SIZE = 0.4;
@@ -156,7 +163,7 @@
             div.style.visibility = "visible";
         }
 
-        if ( !isMobile() ) {
+        if (!isMobile()) {
             // make the splash screen in center
             canvas.width = size.width;
             canvas.height = size.height;
@@ -164,7 +171,7 @@
     }
 
     // init options
-    function initPreviewOptions () {
+    function initPreviewOptions() {
         var defaultDevice = getCookie("device");
         var defaultRotate = getCookie("rotate");
 
@@ -198,7 +205,7 @@
         }
     };
 
-    function onload () {
+    function onload() {
 
         // socket
         // =======================
@@ -210,13 +217,30 @@
             window.location.reload();
         });
         socket.on("browser:confirm-reload", function () {
-            var r = confirm( "Reload?" );
-            if ( r ) {
+            var r = confirm("Reload?");
+            if (r) {
                 window.location.reload();
             }
         });
 
-        function updateResolution () {
+        cc.sys.getSafeAreaRect = function () {
+            const winSize = cc.view.getVisibleSize();
+            const screenSize = getEmulatedScreenSize();
+            if (!isFullScreen() && screenSize.safeWidth && screenSize.safeHeight) {
+                const resolution = cc.Canvas.instance.designResolution;
+                if (rotated) {
+                    const offset = (resolution.width / screenSize.width) * (screenSize.width - screenSize.safeWidth);
+                    return cc.rect(offset, 0, winSize.width - offset, winSize.height);
+                } else {
+                    const offset = (resolution.height / screenSize.height) * (screenSize.height - screenSize.safeHeight);
+                    return cc.rect(0, -offset, winSize.width, winSize.height - offset);
+                }
+            } else {
+                return cc.rect(0, 0, winSize.width, winSize.height);
+            }
+        };
+
+        function updateResolution() {
             var size = isFullScreen() ? document.documentElement.getBoundingClientRect() : getEmulatedScreenSize();
             var gameDiv = document.getElementById("GameDiv");
             gameDiv.style.width = size.width + "px";
@@ -233,7 +257,7 @@
             updateResolution();
         });
 
-        optsDevice.addEventListener( "change", function () {
+        optsDevice.addEventListener("change", function () {
             var idx = optsDevice.value;
             setCookie("device", idx.toString());
             updateResolution();
@@ -333,7 +357,7 @@
             // init assets
             engineInited = true;
 
-            cc.assetManager.loadAny({url: "preview-scene.json", __isNative__: false }, null, function (finish, totalCount) {
+            cc.assetManager.loadAny({ url: "preview-scene.json", __isNative__: false }, null, function (finish, totalCount) {
                 var percent = 100 * finish / totalCount;
                 if (progressBar) {
                     progressBar.style.width = percent.toFixed(2) + "%";
@@ -379,7 +403,7 @@
         _CCSettings.hasResourcesBundle && bundleRoot.push(RESOURCES);
 
         var count = 0;
-        function cb (err) {
+        function cb(err) {
             if (err) return console.error(err);
             count++;
             if (count === bundleRoot.length + 1) {
@@ -398,7 +422,7 @@
         }
     }
 
-    function checkEmptyScene () {
+    function checkEmptyScene() {
         var scene = cc.director.getScene();
         if (scene) {
             if (scene.children.length > 1) {
