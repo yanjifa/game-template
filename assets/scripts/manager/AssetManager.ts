f70757f63551159c206a784efba03213771bc432
alpha <yanjifa2013@qq.com>
2020-11-16 00:15:11 +0800

    :recycle: AssetManager 增加目录引用计数

--- assets/scripts/manager/AssetManager.ts
+++ assets/scripts/manager/AssetManager.ts
@@ -2,6 +2,9 @@ import Bluebird = require("bluebird");
 import BaseSingleton from "../base/BaseSingeton";
 
 export default class AssetManager extends BaseSingleton {
+
+    private loadedResDirMap: Map<string, number> = new Map();
+
     public async setup() {
         console.info("AssetManager setup");
     }
@@ -19,11 +22,19 @@ export default class AssetManager extends BaseSingleton {
     }
 
     public async loadDir(path: string, progressCallback?: (percent: number) => void) {
+        let refNum = this.loadedResDirMap.get(path) || 0;
+        if (refNum > 0) {
+            this.loadedResDirMap.set(path, ++refNum);
+            console.info(`AssetManager already loaded: ${path}`);
+            return;
+        }
+        this.loadedResDirMap.set(path, ++refNum);
         await Bluebird.fromCallback((callback) => {
             cc.resources.loadDir(path, (finish, total, item) => {
                 progressCallback && progressCallback(finish / total);
             }, callback);
         });
+        console.info(`AssetManager loadDir: ${path}`);
     }
 
     public releaseDirs(paths: string[]) {
@@ -31,6 +42,27 @@ export default class AssetManager extends BaseSingleton {
     }
 
     public releaseDir(path: string) {
+        let refNum = this.loadedResDirMap.get(path);
+        if (refNum === null && refNum === undefined) {
+            return;
+        }
+        this.loadedResDirMap.set(path, --refNum);
+        if (refNum > 0) {
+            return;
+        }
         cc.resources.release(path);
+        console.info(`AssetManager releaseDir: ${path}`);
+    }
+
+    public isDirLoaded(path: string) {
+        return this.loadedResDirMap.get(path) > 0;
+    }
+
+    public dumpDirMap() {
+        const data = [];
+        this.loadedResDirMap.forEach((v, k) => {
+            v !== 0 && data.push({ dir: k, refNum: v });
+        });
+        console.table(data);
     }
 }
